1d14d5aebba37b7468849be329af0d8becb58dec
Abdulaziz Alrabiah <38816018+aziz9898@users.noreply.github.com>
2019-06-08 23:35:37 +0300

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by aziz on 08/06/2019.
+//  Copyright © 2019 Aziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
